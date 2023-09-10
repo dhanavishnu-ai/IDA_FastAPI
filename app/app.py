89072dba07de53a2348039c3db9faa04bd7e412a
DHANAVISHNU A <vishnukvka@gmail.com>
2023-09-10 07:57:13 +0530

    updated the URL

--- app/app.py
+++ app/app.py
@@ -5,8 +5,8 @@ from main import demo
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return 'Gradio app is running at /gradio', 200
+# @app.get('/')
+# async def root():
+#     return 'Gradio app is running at /gradio', 200
 
-app = gr.mount_gradio_app(app, demo, path='/gradio')
+app = gr.mount_gradio_app(app, demo, path='/')
